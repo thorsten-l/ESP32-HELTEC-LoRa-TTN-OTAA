504d83e73122cfdf3de9f3ba1b4c0b12bc2b0ea1
Thorsten Ludewig <t.ludewig@gmail.com>
2025-01-18 19:42:22 +0100

    aproximate battery percentage

--- ttn/payload_formatter.js
+++ ttn/payload_formatter.js
@@ -7,6 +7,14 @@ function decodeUplink(input) {
   data.battery += 2.0;
   data.crc8le = input.bytes[3];
 
+  // 100% battery is 4.1V
+  // 0% battery is 3.0V
+  var battery = data.battery;
+  if ( data.battery > 4.1 ) {
+    battery = 4.1;
+  }
+  data.batteryPercentage = (battery - 3.0) / (4.1 - 3.0) * 100.0;
+
   return {
     data: data,
     warnings: [],
