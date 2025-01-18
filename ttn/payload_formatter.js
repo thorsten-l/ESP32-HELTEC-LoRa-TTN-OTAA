e045c66095a6051fc54e11675063c51aa2e5ca2d
Thorsten Ludewig <t.ludewig@gmail.com>
2025-01-18 19:51:42 +0100

    rename battery to batteryVoltage

--- ttn/payload_formatter.js
+++ ttn/payload_formatter.js
@@ -3,14 +3,14 @@ function decodeUplink(input) {
   
   data.preamble = input.bytes[0];
   data.status = input.bytes[1];
-  data.battery = input.bytes[2] / 100.0;
-  data.battery += 2.0;
+  data.batteryVoltage = input.bytes[2] / 100.0;
+  data.batteryVoltage += 2.0;
   data.crc8le = input.bytes[3];
 
   // 100% battery is 4.1V
   // 0% battery is 3.0V
-  var battery = data.battery;
-  if ( data.battery > 4.1 ) {
+  var battery = data.batteryVoltage;
+  if ( battery > 4.1 ) {
     battery = 4.1;
   }
   data.batteryPercentage = (battery - 3.0) / (4.1 - 3.0) * 100.0;
