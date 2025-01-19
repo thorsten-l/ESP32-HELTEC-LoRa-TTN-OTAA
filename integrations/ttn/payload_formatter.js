69af8ed2e35677637cd75830461ac2752c90986d
Thorsten Ludewig <t.ludewig@gmail.com>
2025-01-19 11:53:45 +0100

    round decimal places for batteryPercentage

--- integrations/ttn/payload_formatter.js
+++ integrations/ttn/payload_formatter.js
@@ -3,8 +3,7 @@ function decodeUplink(input) {
   
   data.preamble = input.bytes[0];
   data.status = input.bytes[1];
-  data.batteryVoltage = input.bytes[2] / 100.0;
-  data.batteryVoltage += 2.0;
+  data.batteryVoltage = ( 200.0 + input.bytes[2] ) / 100.0;
   data.crc8le = input.bytes[3];
 
   // 100% battery is 4.1V
@@ -13,7 +12,7 @@ function decodeUplink(input) {
   if ( battery > 4.1 ) {
     battery = 4.1;
   }
-  data.batteryPercentage = (battery - 3.0) / (4.1 - 3.0) * 100.0;
+  data.batteryPercentage = Math.round((battery - 3.0) / (4.1 - 3.0) * 100.0);
 
   return {
     data: data,
