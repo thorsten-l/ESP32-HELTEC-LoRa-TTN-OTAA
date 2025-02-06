4342fe4f0321c9452c16155ec8fa599cf10b2c5a
Thorsten Ludewig <t.ludewig@gmail.com>
2025-02-06 10:21:29 +0100

    no negative battery percentage

--- integrations/ttn/payload_formatter.js
+++ integrations/ttn/payload_formatter.js
@@ -3,16 +3,20 @@ function decodeUplink(input) {
   
   data.preamble = input.bytes[0];
   data.status = input.bytes[1];
-  data.batteryVoltage = ( 200.0 + input.bytes[2] ) / 100.0;
+  data.batteryVoltage = (200.0 + input.bytes[2]) / 100.0;
   data.crc8le = input.bytes[3];
 
   // 100% battery is 4.1V
-  // 0% battery is 3.0V
+  // 0% battery is 2.5V
   var battery = data.batteryVoltage;
   if ( battery > 4.1 ) {
     battery = 4.1;
   }
-  data.batteryPercentage = Math.round((battery - 3.0) / (4.1 - 3.0) * 100.0);
+  data.batteryPercentage =  Math.round((battery - 2.5) / (4.1 - 2.5) * 100.0);
+  if ( data.batteryPercentage < 0 )
+  {
+    data.batteryPercentage = 0;
+  }
 
   return {
     data: data,
